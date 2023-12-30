5789a61fcbf887c24aaa29119ca83738561bde23
Emmanuel L Keeya <ekeeya@dsmagic.com>
2023-12-31 00:38:09 +0300

    -init springboot

---
+++ rsp-consumer/src/main/java/com/oddjobs/rspconsumer/RspConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.oddjobs.rspconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RspConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RspConsumerApplication.class, args);
+	}
+
+}
